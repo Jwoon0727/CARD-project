a27878f034a711513eb5edf388b3e0fa65b13ce4
Jwoon0727 <149401369+Jwoon0727@users.noreply.github.com>
2023-11-09 22:42:56 +0900

    11/9

---
+++ practice/area.py
@@ -0,0 +1,42 @@
+import requests
+import csv
+
+# 카카오 개발자 포털에서 발급받은 API 키를 입력하세요.
+kakao_api_key = '07143ce4b1f0455f37c8685c50a101f0'
+
+# 좌표 지정 (위도, 경도)
+latitude = '37.5665'
+longitude = '126.9780'
+
+# API 호출
+url = f'https://dapi.kakao.com/v2/local/search/category.json?category_group_code=PO3&x={longitude}&y={latitude}&radius=500'
+headers = {'Authorization': f'KakaoAK {kakao_api_key}'}
+response = requests.get(url, headers=headers)
+
+# 응답 확인
+print(f'Status Code: {response.status_code}')
+
+if response.status_code == 200:
+    result = response.json()
+    places = result.get('documents', [])
+
+    for place in places:
+        name = place.get('place_name', '')
+        address = place.get('address_name', '')
+        print(f'업체명: {name}, 주소: {address}')
+
+    # CSV 파일 작성
+    with open('places_data.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        fieldnames = ['업체명', '주소']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for place in places:
+            name = place.get('place_name', '')
+            address = place.get('address_name', '')
+            writer.writerow({'업체명': name, '주소': address})
+
+    print('데이터를 places_data.csv 파일에 저장했습니다.')
+else:
+    print(f'Error: {response.status_code}')
--- practice/kakao.py
+++ practice/kakao.py
@@ -1,11 +1,14 @@
-# 패키지 불러오기
+import csv
 from selenium import webdriver
 import pandas as pd
-import csv
 
+# CSV 파일 작성
+f = open('sample.csv', 'w', encoding='utf-8', newline='')
+wr = csv.writer(f)
+headers = ["업체명", "주소"]
+wr.writerow(headers)
 
-
-
+# Selenium을 사용한 웹 스크래핑
 driver = webdriver.Chrome()
 
 # 검색어 입력 및 검색
@@ -15,59 +18,42 @@ kakao_map_search_url = f"https://map.kakao.com/?q={keyword}"
 driver.get(kakao_map_search_url)
 driver.get(kakao_map_search_url)
 
-ind = 1  # 현재 복사한 순서
-no = 1  # 1~5페이지 중 위치한 곳
-page = 1  # 현재 페이지 번호
-list1 = []  # 결과물이 저장되는 리스트
+ind = 1
+no = 1
+page = 1
 
 while 1:
     try:
-        # 업체명, 주소
-        title = driver.find_element(by='xpath',value = f'//*[@id="info.search.place.list"]/li[{ind}]/div[3]/strong/a[2]').text
-        addr = driver.find_element(by='xpath',value = f'//*[@id="info.search.place.list"]/li[{ind}]/div[5]/div[2]/p[1]').text
-        list1.append([title, addr])
+        title = driver.find_element(by='xpath', value=f'//*[@id="info.search.place.list"]/li[{ind}]/div[3]/strong/a[2]').text
+        addr = driver.find_element(by='xpath', value=f'//*[@id="info.search.place.list"]/li[{ind}]/div[5]/div[2]/p[1]').text
+
+        # 리스트에 추가
+        row = [title, addr]
+        wr.writerow(row)
 
         ind += 1
 
     except:
-        # 더보기 버튼 찾기
-        if driver.find_element(by='xpath',value = f'//*[@id="info.search.place.more"]').is_displayed():
-            driver.find_element(by='xpath',value = f'//*[@id="info.search.place.more"]').click()
+        if driver.find_element(by='xpath', value=f'//*[@id="info.search.place.more"]').is_displayed():
+            driver.find_element(by='xpath', value=f'//*[@id="info.search.place.more"]').click()
             no += 1
             ind = 1
             page += 1
             continue
 
-        # 다음 페이지로 이동
         elif no >= 5:
-            driver.find_element(by='xpath',value = f'//*[@id="info.search.page.next"]').click()
+            driver.find_element(by='xpath', value=f'//*[@id="info.search.page.next"]').click()
             no = 1
             ind = 1
             page += 1
             continue
 
-        # 5페이지 단위마다 다음 페이지 버튼 누르기
-        elif driver.find_element(by='xpath',value = f'//*[@id="info.search.page.no{no+1}"]').is_displayed():
+        elif driver.find_element(by='xpath', value=f'//*[@id="info.search.page.no{no+1}"]').is_displayed():
             no += 1
-            driver.find_element(by='xpath',value = f'//*[@id="info.search.page.no{no}"]').click()
+            driver.find_element(by='xpath', value=f'//*[@id="info.search.page.no{no}"]').click()
             ind = 1
             page += 1
             continue
 
-        # 더이상 이동할수 없을 경우 종료
-        
-  
-   
-
-# list1를 DataFrame으로 변환
-df = pd.DataFrame(list1)
-# Using pandas to write to a CSV file
-df.to_csv('output.csv', index=False)
-# Using the csv module to write to a CSV file
-with open('output.csv', 'w', encoding='utf-8', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(list1)
-    
-    f.close()
-print(df)
-
+# CSV 파일 닫기
+f.close()
