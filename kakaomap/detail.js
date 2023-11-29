6ae30b6d7ac1e307f080822554be591851811f08
Jwoon0727 <149401369+Jwoon0727@users.noreply.github.com>
2023-11-29 20:45:28 +0900

    11/29

--- kakaomap/detail.js
+++ kakaomap/detail.js
@@ -1,68 +1,68 @@
-// overlay_content.js
+// // overlay_content.js
 
-// 오버레이 내용을 생성하는 함수
-function createOverlayContent(markerInfo) {
-    return `
-        <div class="wrap">
-            <div class="info">
-                <div class="">
-                    ${markerInfo.title}
-                    <div class="close" onclick="closeOverlay()" title="${markerInfo.closeTitle}"></div>
-                </div>
-                <div class="body">
-                    <div class="img">
-                        <img src="${markerInfo.imgSrc}" width="73" height="70">
-                    </div>
-                    <div class="desc">
-                        <div class="ellipsis">${markerInfo.address}</div>
-                        <div class="jibun ellipsis">${markerInfo.postalCode}</div>
+// // 오버레이 내용을 생성하는 함수
+// function createOverlayContent(markerInfo) {
+//     return `
+//         <div class="wrap">
+//             <div class="info">
+//                 <div class="">
+//                     ${markerInfo.title}
+//                     <div class="close" onclick="closeOverlay()" title="${markerInfo.closeTitle}"></div>
+//                 </div>
+//                 <div class="body">
+//                     <div class="img">
+//                         <img src="${markerInfo.imgSrc}" width="73" height="70">
+//                     </div>
+//                     <div class="desc">
+//                         <div class="ellipsis">${markerInfo.address}</div>
+//                         <div class="jibun ellipsis">${markerInfo.postalCode}</div>
                         
-                    </div>
-                </div>
-            </div>
-        </div>`;
-}
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>`;
+// }
 
-// inner_marker.js
+// // inner_marker.js
 
-// 마커를 추가하는 함수
-function addMarker(map, position, markerName, additionalInfo) {
-   // 오버레이 내용을 생성
-    const markerInfo = {
-        title: markerName,
-        closeTitle: '닫기',
-        imgSrc: additionalInfo.imgSrc,
-        address: additionalInfo.address,
-        postalCode: additionalInfo.postalCode,
-        homepage: additionalInfo.homepage
-    };
-    const content = createOverlayContent(markerInfo);
+// // 마커를 추가하는 함수
+// function addMarker(map, position, markerName, additionalInfo) {
+//    // 오버레이 내용을 생성
+//     const markerInfo = {
+//         title: markerName,
+//         closeTitle: '닫기',
+//         imgSrc: additionalInfo.imgSrc,
+//         address: additionalInfo.address,
+//         postalCode: additionalInfo.postalCode,
+//         homepage: additionalInfo.homepage
+//     };
+//     const content = createOverlayContent(markerInfo);
 
-    // 마커 생성
-    const marker = new kakao.maps.Marker({
-        position: position,
-        map: map,
-        title: markerName
-    });
+//     // 마커 생성
+//     const marker = new kakao.maps.Marker({
+//         position: position,
+//         map: map,
+//         title: markerName
+//     });
 
-    // 오버레이 추가 함수 호출 시 마커와 인포윈도우에 표시할 내용(content) 전달
-    addOverlay(map, marker, content);
-}
+//     // 오버레이 추가 함수 호출 시 마커와 인포윈도우에 표시할 내용(content) 전달
+//     addOverlay(map, marker, content);
+// }
 
-// 폴리곤 파일의 전역 변수인 globalMap을 사용하여 마커를 추가
-kakao.maps.load(() => {
-    // addMarker 함수 호출 시 globalMap을 전달 및 마커에 표시할 내용(content) 전달
-    addMarker(globalMap, new kakao.maps.LatLng(36.832408, 127.147905), '구역번호', {
-        imgSrc: '이미지_경로_1',
-        address: '주소_1',
-        postalCode: '우편번호_1',
-        homepage: '홈페이지_주소_1'
-    });
-    addMarker(globalMap, new kakao.maps.LatLng(36.828, 127.125), '구역번호', {
-        imgSrc: '이미지_경로_2',
-        address: '주소_2',
-        postalCode: '우편번호_2',
-        homepage: '홈페이지_주소_2'
-    });
-    // 원하는 만큼 마커를 추가할 수 있음
-});
+// // 폴리곤 파일의 전역 변수인 globalMap을 사용하여 마커를 추가
+// kakao.maps.load(() => {
+//     // addMarker 함수 호출 시 globalMap을 전달 및 마커에 표시할 내용(content) 전달
+//     addMarker(globalMap, new kakao.maps.LatLng(36.832408, 127.147905), '구역번호', {
+//         imgSrc: '이미지_경로_1',
+//         address: '주소_1',
+//         postalCode: '우편번호_1',
+//         homepage: '홈페이지_주소_1'
+//     });
+//     addMarker(globalMap, new kakao.maps.LatLng(36.828, 127.125), '구역번호', {
+//         imgSrc: '이미지_경로_2',
+//         address: '주소_2',
+//         postalCode: '우편번호_2',
+//         homepage: '홈페이지_주소_2'
+//     });
+//     // 원하는 만큼 마커를 추가할 수 있음
+// });
--- kakaomap/inner_marker.js
+++ kakaomap/inner_marker.js
@@ -1,21 +1,51 @@
 // inner_marker.js
 
-// 오버레이를 추가하는 함수
+// 지도에 사용자 정의 오버레이를 추가하는 함수
 function addOverlay(map, marker, content) {
-    // 인포윈도우 생성
-    const infowindow = new kakao.maps.InfoWindow({
+    const overlay = new kakao.maps.CustomOverlay({
         content: content,
-        removable: true
+        map: map,
+        position: marker.getPosition(),
     });
 
-    // 마커 클릭 이벤트 리스너 등록
+    overlay.setMap(null);
+
+    // 마커를 클릭했을 때 오버레이를 표시 또는 숨기는 이벤트 추가
     kakao.maps.event.addListener(marker, 'click', function() {
-        // 인포윈도우를 마커 위에 표시
-        infowindow.open(map, marker);
+        if (overlay.getMap()) {
+            overlay.setMap(null); // 이미 보이는 경우 숨김
+        } else {
+            overlay.setMap(map); // 보이지 않는 경우 표시
+        }
     });
+
+   
+    
 }
 
-// 마커를 추가하는 함수
+
+
+// 커스텀 오버레이에 표시할 컨텐츠
+var content = '<div class="wrap">' +
+    '    <div class="info">' +
+    '        <div class="title">' +
+    '            카카오 스페이스닷원' +
+    '        </div>' +
+    '        <div class="body">' +
+    '            <div class="img">' +
+    '                <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png" width="73" height="70">' +
+    '           </div>' +
+    '            <div class="desc">' +
+    '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+    '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+    '                <div><a href="sub.html" class="link" onclick="window.location.href=\'sub.html\';">정보</a></div>' +
+    '            </div>' +
+    '        </div>' +
+    '    </div>' +
+    '</div>';
+
+
+// 지도에 사용자 정의 오버레이가 있는 마커를 추가하는 함수
 function addMarker(map, position, content) {
     // 마커 생성
     const marker = new kakao.maps.Marker({
@@ -24,14 +54,14 @@ function addMarker(map, position, content) {
         title: '마커 제목'
     });
 
-    // 오버레이 추가 함수 호출 시 마커와 인포윈도우에 표시할 내용(content) 전달
+    // 오버레이 추가 함수 호출 시 마커와 표시할 내용(content) 전달
     addOverlay(map, marker, content);
 }
 
 // 폴리곤 파일의 전역 변수인 globalMap을 사용하여 마커를 추가
 kakao.maps.load(() => {
     // addMarker 함수 호출 시 globalMap을 전달 및 마커에 표시할 내용(content) 전달
-    addMarker(globalMap, new kakao.maps.LatLng(36.832408, 127.147905), '마커 1의 내용');
-    addMarker(globalMap, new kakao.maps.LatLng(36.828, 127.125), '마커 2의 내용');
+    addMarker(globalMap, new kakao.maps.LatLng(36.832408, 127.147905), content);
+    addMarker(globalMap, new kakao.maps.LatLng(36.828, 127.125), content);
     // 원하는 만큼 마커를 추가할 수 있음
 });
