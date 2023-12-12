180a1393a4b74b524e709bfb022489877cc35fa4
Jwoon0727 <149401369+Jwoon0727@users.noreply.github.com>
2023-12-12 21:41:58 +0900

    12/12

--- kakaomap/overlay.js
+++ kakaomap/overlay.js
@@ -1,43 +1,116 @@
+
 // overlay.js
+var locations = [
+    { 
+        number: 222, 
+        lat: 36.8278461, 
+        lng: 127.1243682, 
+        polygonPath: [
+            new kakao.maps.LatLng(36.8290402, 127.1228792),
+            new kakao.maps.LatLng(36.8267373, 127.1229472),
+            new kakao.maps.LatLng(36.8265951, 127.1272716),
+            new kakao.maps.LatLng(36.8274807, 127.1272790),
+            new kakao.maps.LatLng(36.8274571, 127.1266849),
+            new kakao.maps.LatLng(36.8283151, 127.1266551),
+            new kakao.maps.LatLng(36.8287501, 127.1261833),
+            new kakao.maps.LatLng(36.8289688, 127.1260737),
+            new kakao.maps.LatLng(36.8290402, 127.1228792)
+        ]
+    },
+    { 
+        number: 223, 
+        lat: 36.8250447, 
+        lng: 127.1214670, 
+        polygonPath: [
+            new kakao.maps.LatLng(36.8268363, 127.1208356),
+            new kakao.maps.LatLng(36.8265659, 127.1201084),
+            new kakao.maps.LatLng(36.8263251, 127.1197524),
+            new kakao.maps.LatLng(36.8261136, 127.1196652),
+            new kakao.maps.LatLng(36.8259234, 127.1190198),
+            new kakao.maps.LatLng(36.8239221, 127.1185705),
+            new kakao.maps.LatLng(36.8229100, 127.1182483),
+            new kakao.maps.LatLng(36.8235911, 127.1238766),
+            new kakao.maps.LatLng(36.8244845, 127.1237908),
+            new kakao.maps.LatLng(36.8267719, 127.1236938),
+            new kakao.maps.LatLng(36.8267419, 127.1211079),
+            new kakao.maps.LatLng(36.8268363, 127.1208356)
+            // 다른 폴리곤 좌표 추가
+        ]
+    },
+    // 필요한 만큼 더 많은 위치를 추가하세요
+];
 
-// 전역 변수로 선언된 customOverlay
-var customOverlay;
+// 초기화할 때 지도에 표시할 폴리곤 객체 배열
+var polygons = [];
 
-// Custom overlay를 생성하고 설정하는 함수
-function createCustomOverlay(info, position) {
-    // 기존 오버레이가 존재하는 경우 제거
-    if (customOverlay) {
-        customOverlay.setMap(null);
+
+function searchLocation() {
+    var keyword = document.getElementById('searchInput').value;
+    var foundLocation = locations.find(location => location.number.toString() === String(keyword));
+
+    if (foundLocation) {
+        // 이동할 좌표 설정
+        var moveLatLng = new kakao.maps.LatLng(foundLocation.lat, foundLocation.lng);
+
+        // 검색된 좌표로 지도 이동
+        map.panTo(moveLatLng);
+
+        // 마커 표시
+        marker.setPosition(moveLatLng);
+        marker.setMap(map);
+
+        // 인포윈도우 닫기
+        infowindow.close();
+
+        // 폴리곤 표시
+        displayPolygons(foundLocation);
+    } else {
+        // 다른 검색 기능을 추가하거나 사용자에게 메시지 표시
+        alert('입력한 번호에 대한 결과를 찾을 수 없습니다.');
+
+        // 폴리곤 지우기
+        clearPolygons();
     }
-
-    customOverlay = new kakao.maps.CustomOverlay({
-        position: position,
-        content: '<div class="wrap">' +
-            '    <div class="info">' +
-            '        <div class="title">' +
-            '            ' + info.title +
-            '         <div class="close" onclick="closeOverlay()" title="닫기">닫기</div>' +
-            '        </div>' +
-            '        <div class="body">' +
-            '            <div class="desc">' +
-            '                <div class="ellipsis">' + info.address + '</div>' +
-            '                <div class="jibun ellipsis">' + info.postalCode + '</div>' +
-            '                <div><a href="' + info.homepage + '" target="_blank" class="link">홈페이지</a></div>' +
-            '            </div>' +
-            '        </div>' +
-            '    </div>' +
-            '</div>',
-        xAnchor: 0.5,
-        yAnchor: 1,
-    });
-
-    // Custom overlay를 지도에 표시합니다
-    customOverlay.setMap(map);
 }
 
-// 오버레이를 닫는 함수
+function displayPolygons(location) {
+    // 기존 폴리곤 지우기
+    clearPolygons();
+
+    // 현재 위치에 대한 폴리곤 표시
+    var polygon = new kakao.maps.Polygon({
+        path: location.polygonPath,
+        strokeWeight: 3,
+        strokeColor: 'red',
+        strokeOpacity: 0.8,
+      
+    });
+
+    // 폴리곤 지도에 추가
+    polygon.setMap(map);
+
+    // 배열에 추가하여 추후에 제거할 때 활용
+    polygons.push(polygon);
+}
+
+function clearPolygons() {
+    // 배열에 저장된 모든 폴리곤 지도에서 제거
+    polygons.forEach(polygon => polygon.setMap(null));
+    
+    // 배열 비우기
+    polygons = [];
+}
+
 function closeOverlay() {
-    if (customOverlay) {
-        customOverlay.setMap(null); // 지도에서 오버레이를 제거합니다
-    }
+    infowindow.close();
+    marker.setMap(null);
+
+    // 폴리곤 지우기
+    clearPolygons();
+}
+function closeOverlay() {
+    infowindow.close();
+    marker.setMap(null);
+    // 폴리곤을 지도에서 제거하지 않음 (유지)
+    // clearPolygons();
 }
